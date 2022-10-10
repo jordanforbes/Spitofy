e2ce4289fd2705c1dd7c3996761c128cf0d92c17
jordanforbes <jordforb@gmail.com>
2022-10-10 15:27:38 -0400

    songcards

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -2,12 +2,20 @@ import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
-    baseQuery:({
+    baseQuery: fetchBaseQuery({
         baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
         prepareHeaders: (headers)=>{
-            headers.set('X-RapidAPI-Key', '559fdc3e47mshbe942c02fc66a7dp13d1edjsn0d3bdb06f8a5')
+            headers.set('X-RapidAPI-Key',
+            '559fdc3e47mshbe942c02fc66a7dp13d1edjsn0d3bdb06f8a5');
 
             return headers;
-        }
-    })
-})
+        },
+    }),
+    endpoints: (builder) =>({
+        getTopCharts: builder.query({ query: () => '/charts/world'}),
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
